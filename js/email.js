d65828055085ecd3074e952b892b1f7ad5e5f1ef
jakobowsky <jakob.wolitzki@gmail.com>
2019-08-17 10:53:27 +0200

    email form done

--- js/email.js
+++ js/email.js
@@ -1,8 +1,26 @@
 function handleSubmit() {
-    const email = document.getElementById("email").value;
-    const comment = document.getElementById("comment").value;
-    postData({ email, comment }).then(data => console.log(JSON.stringify(data))).catch(error => console.error(error));
+    const name = document.getElementById("form_name").value;
+    const email = document.getElementById("form_email").value;
+    const comment = document.getElementById("form_message").value;
+    var $result = $("#result");
+    if (validateEmail(email)) {
+        $result.text("Dziekujemy, udało Ci się wysłać wiadomość.");
+        $result.css("color", "green");
+        postData({ name, email, comment }).then(data => console.log(JSON.stringify(data))).catch(error => console.error(error));
+        return true;
+    } 
+    else {
+        $result.text("email jest niepoprawny.");
+        $result.css("color", "red");
+        return false;
+    }
+   
 }
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
 function postData(data = {}) {
     const url = 'https://g1q5hl6xsg.execute-api.us-east-2.amazonaws.com/default/send_email';
